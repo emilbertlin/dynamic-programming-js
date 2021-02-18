669d4c0a252c282471a9beb59906bf43be31719b
emil bertlin <emilbertlin@outlook.com>
2021-02-18 20:22:02 +0100

    first commit

---
+++ fib.js
@@ -0,0 +1,18 @@
+// Memoization
+//   JS object, keys will be arg to func, value will be the return value
+
+
+const fib = (n, memo = {}) => {
+  if (n in memo) return memo[n];
+  if (n <= 2) return 1;
+  memo[n] = fib(n - 1, memo) + fib(n - 2, memo);
+  return memo[n];
+}
+
+// 0(n) time
+// 0(n) space
+
+console.log(fib(6))
+console.log(fib(7))  
+console.log(fib(8))  
+console.log(fib(100))  
---
+++ gridTraveler.js
@@ -0,0 +1,45 @@
+// m, n represents a grid. 
+// Your starting point is top left of the grid.
+// You can only move down or right in the grid. 
+// Find number of steps required to get from top left to bottom right.
+
+// a [0, n] or [n, 0] grid is invalid since it's one dimensional therefore return 0
+// in a [1, 1] grid you cant do anything therefore return 1
+
+{/*
+const gridTraveler = (m, n) => {
+  if (m === 1 && n === 1) return 1;
+  if (m === 0 || n === 0)return 0;
+  return gridTraveler(m - 1, n) + gridTraveler(m, n - 1);
+}
+
+console.log(gridTraveler(1, 1)) // 1
+console.log(gridTraveler(2, 3)) // 3
+console.log(gridTraveler(3, 2)) // 3
+console.log(gridTraveler(3, 3)) // 6
+console.log(gridTraveler(18, 18)) // 2333606220
+*/}
+
+// 0(2^(n+m)) time
+// 0(n + m) space
+
+
+// gridTravelerMemoized
+const gridTraveler = (m, n, memo = {}) => {
+  const key = m + ',' + n;
+  if (key in memo) return memo[key];
+  if (m === 1 && n === 1) return 1;
+  if (m === 0 || n === 0) return 0;
+
+  memo[key] = gridTraveler(m - 1, n, memo) + gridTraveler(m, n - 1, memo);
+  return memo[key];
+}
+
+console.log(gridTraveler(1, 1)) // 1
+console.log(gridTraveler(2, 3)) // 3
+console.log(gridTraveler(3, 2)) // 3
+console.log(gridTraveler(3, 3)) // 6      
+console.log(gridTraveler(18, 18)) // 2333606220
+
+// O(m * n) time
+// O(m + n) space
